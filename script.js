f99980e7f6197588dca701c6ad58a862f2a85aa9
shivar007 <mr.adarsh2709@gmail.com>
2025-03-05 10:05:13 +0530

    added index.html, script.js and style.css

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeGreeting() {
+    const nameElement = document.getElementById("name");
+    const wishNote = document.querySelector(".wish-note");
+    
+    nameElement.textContent = "Abhishree"; // Change "John Doe" to the actual birthday person's name.
+    wishNote.textContent = "Happy Birthday, Pari! 🎉🌸 Wishing you a day filled with love, laughter, and all your favorite things. May this year be your best one yet 🎂✨💖! From Shiva, Tum jo kuch chao sab kuch tumhe mile aur aunty bhi movie dekhne jane ka permisson dede 💖🤣🎉🎂💐✨💫!!";
+}
